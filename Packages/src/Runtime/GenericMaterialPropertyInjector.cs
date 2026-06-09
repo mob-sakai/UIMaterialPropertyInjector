58dd308691b4bfc8c972daebddc9073f210ac63a
mob-sakai <12690315+mob-sakai@users.noreply.github.com>
2026-06-09 20:10:50 +0900

    update internal code

--- Packages/src/Runtime/GenericMaterialPropertyInjector.cs
+++ Packages/src/Runtime/GenericMaterialPropertyInjector.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Profiling;
+using Coffee.UIMaterialPropertyInjectorInternal;
 
 namespace Coffee.UIExtensions
 {
--- Packages/src/Runtime/UIMaterialPropertyTweener.cs
+++ Packages/src/Runtime/UIMaterialPropertyTweener.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Coffee.UIMaterialPropertyInjectorInternal;
 
 namespace Coffee.UIExtensions
 {
